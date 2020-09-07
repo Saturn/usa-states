587dfd76ed8bd53002be2f62f6a6aeb30a217e51
Patrick McMichael <patrick@patrickmcmichael.org>
2020-09-07 15:45:59 +0100

    Update build

---
+++ precache-manifest.a0686185b6d6447922d9c40257d8c810.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c893cc9e191c5b8c650",
+    "url": "./static/css/main.9c651bc4.chunk.css"
+  },
+  {
+    "revision": "8c893cc9e191c5b8c650",
+    "url": "./static/js/main.8c893cc9.chunk.js"
+  },
+  {
+    "revision": "2df3cca116b923c82692",
+    "url": "./static/js/1.2df3cca1.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "c8991c4be4f13103fef11dcbfdb0eb72",
+    "url": "./index.html"
+  }
+];
